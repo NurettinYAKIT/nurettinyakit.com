ea0b8e9231655f4e0817dfe75c706ba374d58688
nurettinyakit <onuryakit@gmail.com>
2018-12-03 08:48:04 +0100

    Angular Docker | How to Dockerize an Angular App | http://www.nurettinyakit.com/2018/12/how-to-dockerize-angular-7-app.html

---
+++ fe-stock-angular/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Finance App';
+  apple = '178.58';
+  google = '1,094.43';
+  nvidia = '163.43';
+}
